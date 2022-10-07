b68e21d07699f07fabaf36844e71006634533250
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2022-10-07 16:22:31 -0300

    Adicionando o Projeto lista de filmes

---
+++ ListaDeFilmes/Meus filmes teste/Filme.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class Filme{
+    var titulo: String!
+    var descricao: String!
+    var imagem: UIImage!
+    
+    init(titulo: String, descricao: String, imagem: UIImage) {
+        self.titulo = titulo
+        self.descricao = descricao
+        self.imagem = imagem
+    }
+    
+}
