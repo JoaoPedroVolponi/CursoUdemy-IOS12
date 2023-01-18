53a6ff863addcd232bb77d13b22d8c3f0e0dd607
João Pedro Rosada Volponi <jpvrosada@hotmail.com>
2023-01-18 16:33:23 -0300

    Adicionando o Projeto Instagram

---
+++ Instagram/Instagram/TableViewCell/GaleriaCollectionViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class GaleriaCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var foto: UIImageView!
+    
+    @IBOutlet weak var descricao: UILabel!
+    
+}
---
+++ Instagram/Instagram/TableViewCell/PostagemTableViewCell.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class PostagemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var descricaoPostagem: UILabel!
+    @IBOutlet weak var fotoPostagem: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
